a849e768318ef5a69f39fb0fd51f4112621b9dbf
username <useremail>
2024-03-13 14:12:29 -0700

    Created view and add properties.

--- Counry.py
+++ Counry.py
@@ -1,3 +1,41 @@
 # Braden Jackson
 # CIS261
 # Lab Submission: Country
+
+def dispay_menu():
+    print("Command MENU")
+    print("view - View contry name")
+    print("add  - Add a country")
+    print("del  - Delete a country")
+    print("exit - Exit program")
+    
+def display_codes(countries):
+    codes = list(countries.keys())
+    codes.sort()
+    codes_line = "Country codes: "
+    for code in codes:
+        codes_line += code + " "   
+    print(codes_line)
+    
+def view(countries):
+    display_codes(countries)
+    code = input("Enter country code: ")
+    code = code.upper()
+    if code in countries:
+        name = countries[code]
+        print(f"Country name: {name}.\n")
+    else:
+        print("There is no country with that code.\n")
+        
+def add(countries):
+    code = input("Enter country code: ")
+    code = code.upper()
+    if code in countries:
+        name = countries[code]
+        print(f"{name} is already using this code.\n")
+    else:
+        name = input("Enter country name: ")
+        name = name.titile()
+        countries[code] = name
+        print(f"{name} was added.\n")
+        
